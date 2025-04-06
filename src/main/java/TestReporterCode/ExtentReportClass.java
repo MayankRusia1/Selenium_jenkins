bab93f7adec5f2c82144599f4e758bcb68a44fdf
Mayank Rusia <mayankrusia97@gmail.com>
2025-04-06 18:38:24 +0530

    fourth commit

--- src/main/java/TestReporterCode/ExtentReportClass.java
+++ src/main/java/TestReporterCode/ExtentReportClass.java
@@ -1,5 +1,8 @@
 package TestReporterCode;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -7,8 +10,10 @@ public class ExtentReportClass
 {
 	public static ExtentReports getReporter(String filename)
 	{
+		String timestamp = new SimpleDateFormat("dd.MM.yyyy.hh.mm.ss").format(new Date());
+		
 		ExtentReports extentReports = new ExtentReports();
-		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir")+"\\TestReports_folder\\"+filename+".html");
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir")+"\\TestReports_folder\\"+filename+"_"+timestamp+".html");
 		extentReports.attachReporter(sparkReporter);
 		return extentReports;
 	}
